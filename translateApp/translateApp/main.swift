83c5e43809a264f03a55ab90fd4249807bab0e60
Ovanes <o-zakharyan@mail.ru>
2021-02-04 19:25:23 +0300

    func

---
+++ translateApp/translateApp/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  translateApp
+//
+//  Created by Ovanes on 04.02.2021.
+//
+
+import Foundation
+
+
+
+func translate(_ wordsEnglish: [String], _ wordsRussian: [String]) {
+    
+}
+
