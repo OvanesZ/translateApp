80259717f41eeba8cadb2223587ed6a41d589818
Ovanes <o-zakharyan@mail.ru>
2021-02-04 19:37:13 +0300

    final_func

--- translateApp/translateApp/main.swift
+++ translateApp/translateApp/main.swift
@@ -11,5 +11,26 @@ import Foundation
 
 func translate(_ wordsEnglish: [String], _ wordsRussian: [String]) {
     
+    var newWordsEnglishArr = wordsEnglish
+    var newWordsRussianArr = wordsRussian
+    
+    while newWordsEnglishArr.count != 0 {
+        
+        var randomElement = Int.random(in: 0...newWordsEnglishArr.count-1)
+        print("Как переводится \(newWordsEnglishArr[randomElement])?")
+        var userAnswer: String = readLine(strippingNewline: true)!
+        if userAnswer == newWordsRussianArr[randomElement] {
+            print("Правильно!")
+            newWordsEnglishArr.remove(at: randomElement)
+            newWordsRussianArr.remove(at: randomElement)
+        } else {
+            print("Неправильно!")
+        }
+    }
 }
+
+var wordsEnglishArr: [String] = ["food", "hello", "world"]
+var wordsRussianArr: [String] = ["еда", "привет", "мир"]
+
+translate(wordsEnglishArr, wordsRussianArr)
 
